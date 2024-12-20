489bc9b7acd0a7063b6273bc23025f488eaeef7d
Mass-14 <126057141+Mass-14@users.noreply.github.com>
2024-12-20 22:29:22 +0100

    tabs on right post

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -134,40 +134,7 @@ let BeautifulJekyllJS = {
         $("body").removeClass("overflow-hidden");
       }
     });
-  },
-
-
-  openTab : function(event, tabName) {
-    // Hide all tab contents
-  const allTabs = document.querySelectorAll('.tab-content');
-  allTabs.forEach(tab => {
-    tab.classList.remove('active');
-  });
-
-  // Remove active class from all tabs
-  const allTabLinks = document.querySelectorAll('.tab');
-  allTabLinks.forEach(tab => {
-    tab.classList.remove('active');
-  });
-
-  // Show the clicked tab's content
-  const activeTabContent = document.getElementById(tabName);
-  activeTabContent.classList.add('active');
-
-  // Add active class to the clicked tab
-  event.currentTarget.classList.add('active');
-
-
-  // Optionally, make the first tab active by default
-  document.addEventListener('DOMContentLoaded', () => {
-    const firstTab = document.querySelector('.tab');
-    if (firstTab) firstTab.click(); // Trigger click to activate the first tab
-    });
-
   }
-
-
-
   
 };
 
